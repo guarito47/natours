acee62484c34b4cb27f5d9db0b4b391e26a5acac
Edwin Guarachi <eguarachi@outlook.com>
2025-04-18 16:32:37 +0000

    Updated tourController.js to trigger demo

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,7 @@ exports.getAllTours= async(req, res)=>{
     res
     .status(200)
     .json({
-        status:'success v417pm',
+        status:'success v418pm',
     
         results: tours.length,
         data: {
